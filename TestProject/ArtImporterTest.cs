4ae9e2bca24e6ff82c9e7873c786a80d5468b9cd
Brian Preston <brian8181@gmail.com>
2008-11-21 14:30:21 +0000

    git-svn-id: file:///home/brian/mig/music_importer/trunk@79 590353e8-683b-44f6-997f-b61536f82bd4

--- TestProject/ArtImporterTest.cs
+++ TestProject/ArtImporterTest.cs
@@ -1,8 +1,10 @@
-﻿using MusicImporter_Lib;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using TagLib;
+﻿using System.Text.RegularExpressions;
 using BKP.Online.Data;
-using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicImporter_Lib;
+using TagLib;
+using System.IO;
+using MySql.Data.MySqlClient;
 
 namespace TestProject
 {
@@ -86,6 +88,59 @@ namespace TestProject
             string exp = @"^\{[a-f0-9]{8}-([a-f0-9]{4}-){3}[a-f0-9]{12}\}.\w+$";
             Regex regex = new Regex(exp);
             StringAssert.Matches(actual, regex);
+        }
+
+        /// <summary>
+        ///A test for DeleteOrphanedInserts
+        ///</summary>
+        [TestMethod()]
+        public void DeleteOrphanedInsertsTest()
+        {
+            IDatabase db = Globals.MySQL_DB; // TODO: Initialize to an appropriate value
+            string art_path = "..\\..\\";
+            
+            string file = string.Format("{0}test1.jpg ",  art_path);
+            FileInfo fi = new FileInfo(file);
+            // create then close stream
+            fi.Create().Close();
+            art_path = fi.DirectoryName;
+           
+            // set up db
+            db.ChangeDatabase("music_test");
+            db.ExecuteNonQuery("TRUNCATE song_art");
+            db.ExecuteNonQuery("TRUNCATE art");
+            db.ExecuteNonQuery("TRUNCATE song");
+            
+            // entry 1
+            db.ExecuteNonQuery("INSERT INTO song (`file`) VALUES ('duh.jpg')");
+            object sid = db.LastInsertID;
+            db.ExecuteNonQuery("INSERT INTO art VALUES (NULL, 'duh.jpg', NULL, NULL, NULL, NULL, NULL, NULL)");
+            object aid = db.LastInsertID;
+            
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO song_art (`song_id`, `art_id`) VALUES (?sid, ?aid)");
+            cmd.Parameters.AddWithValue("?sid", sid);
+            cmd.Parameters.AddWithValue("?aid", aid);
+            db.ExecuteNonQuery( cmd );
+
+            // entry 2
+            db.ExecuteNonQuery("INSERT INTO song (`file`) VALUES ('test1.jpg')");
+            sid = db.LastInsertID;
+            db.ExecuteNonQuery("INSERT INTO art VALUES (NULL, 'test1.jpg', NULL, NULL, NULL, NULL, NULL, NULL)");
+            aid = db.LastInsertID;
+
+            cmd = new MySqlCommand("INSERT INTO song_art (`song_id`, `art_id`) VALUES (?sid, ?aid)");
+            cmd.Parameters.AddWithValue("?sid", sid);
+            cmd.Parameters.AddWithValue("?aid", aid);
+            db.ExecuteNonQuery(cmd);
+             
+            // the test! 
+            // two songs with two arts links have been inserted, the following
+            // attempts to delete the art and the links because its has 
+            // no matching file ( duh.jpg )
+            ArtImporter target = new ArtImporter(db, art_path); // TODO: Initialize to an appropriate value
+            uint deleted = target.DeleteOrphanedInserts(art_path);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(deleted == 1);
+            System.IO.File.Delete(file);
         }
     }
 }
