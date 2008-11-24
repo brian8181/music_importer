4f3beae15e1a93be4111a04feac0ffe98baaf5f9
Brian Preston <brian8181@gmail.com>
2008-11-24 12:46:45 +0000

    git-svn-id: file:///home/brian/mig/music_importer/trunk@86 590353e8-683b-44f6-997f-b61536f82bd4

---
+++ GUI/sql/updates/update.1.0.2.sql
@@ -0,0 +1,22 @@
+﻿DROP PROCEDURE IF EXISTS `get_artist_by_letter`;
+CREATE PROCEDURE `get_artist_by_letter`(IN letter CHAR, filter TEXT)
+
+BEGIN
+
+IF letter <> 'T' THEN
+
+SELECT DISTINCT artist.id, artist FROM artist INNER JOIN song ON artist_id=artist.id
+  WHERE artist like CONCAT(letter, '%') AND song.file LIKE CONCAT(filter, '%')
+    UNION
+SELECT artist.id, SUBSTRING(artist.artist, 5, LENGTH(artist.artist) - 1) FROM artist INNER JOIN song ON artist_id=artist.id
+    WHERE artist like CONCAT('The ', letter, '%') AND song.file LIKE CONCAT(filter, '%') ORDER BY artist;
+
+ELSE
+
+  SELECT DISTINCT artist.id, artist FROM  artist INNER JOIN song ON artist_id=artist.id
+    WHERE ((artist LIKE 'T%' AND artist NOT LIKE 'The %') OR (artist LIKE 'The T%'))
+    AND song.file LIKE CONCAT(filter, '%') ORDER BY artist;
+
+END IF;
+
+END
